9751ab089dfedbc5643b330e6d3f2d44bf974a5e
Alex Pauwels <pauwels-a@outlook.com>
2023-05-03 18:02:39 +0100

    Responsive updates

--- app/events/forms.py
+++ app/events/forms.py
@@ -3,5 +3,3 @@ from wtforms import StringField, IntegerField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email
 from flask_babel import _
 from app.events.qteam.forms import (QteamForm)
-
-
