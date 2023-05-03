bd6cdea8b486ae88180f505b771c36f2f070bd41
Alex Pauwels <pauwels-a@outlook.com>
2023-05-03 17:22:53 +0100

    Q-team part

--- app/events/forms.py
+++ app/events/forms.py
@@ -2,41 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email
 from flask_babel import _
+from app.events.qteam.forms import (QteamForm)
 
 
-how_many_centers_choices = [
-    ('0', _('Bijna 50')),
-    ('1', _('Bijna 70')),
-    ('2', _('Bijna 100'))
-]
-which_type_car_choices = [
-    ('0', _('Particuliere wagen')),
-    ('1', _('Particuliere wagen en bedrijfswagen')),
-    ('2', _('Particuliere wagen, bedrijfswagen en leasewagen'))
-]
-when_summer_tires_choices = [
-    ('0', _('2 graden celsius')),
-    ('1', _('7 graden celsius')),
-    ('2', _('15 graden celcius'))
-]
-
-class QteamForm(FlaskForm):
-    how_many_centers = SelectField(
-        _('Hoeveel Q-Team service centers zijn er in België aanwezig'),
-        choices=how_many_centers_choices,
-        validators=[DataRequired()])
-    which_type_car = SelectField(
-        _('Met welk type wagen kan je bij een QTeam service center terecht'),
-        choices=which_type_car_choices,
-        validators=[DataRequired()])
-    when_summer_tires = SelectField(
-        _('Vanaf wanneer is het aangeraden om op zomerbanden om te schakelen'),
-        choices=when_summer_tires_choices,
-        validators=[DataRequired()])
-    how_many_people = IntegerField(
-        _('Hoeveel personen zullen aan deze competitie deelnemen'),
-         validators=[DataRequired()])
-    email = StringField(_('Email'), validators=[DataRequired(), Email()])
-    first_name = StringField(_('Voornaam'), validators=[DataRequired()])
-    last_name = StringField(_('Familienaam'), validators=[DataRequired()])
-    submit = SubmitField(_('Indienen'))
---
+++ app/events/qteam/routes.py
@@ -0,0 +1,37 @@
+from flask_babel import _, get_locale
+from langdetect import detect, LangDetectException
+from datetime import datetime
+from flask import render_template, url_for, redirect, request, current_app, g
+from app.events import bp
+from app.events.forms import QteamForm
+from app.models import QteamEvent
+from app import db
+
+@bp.route('/q-team/', methods=['GET', 'POST'])
+def index():
+    form = QteamForm()
+    if form.validate_on_submit():
+        entry = QteamEvent(
+            how_many_centers=form.how_many_centers.data,
+            which_type_car=form.which_type_car.data,
+            when_summer_tires=form.when_summer_tires.data,
+            how_many_people=form.how_many_people.data,
+            email=form.email.data,
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            created=datetime.utcnow()
+        )
+        db.session.add(entry)
+        db.session.commit()
+        
+        return redirect(url_for('events.success'))
+    elif request.method == 'GET': 
+        return render_template('events/index.html', title=(_('Welcome')), form=form)
+
+@bp.route('/q-team/success', methods=['GET'])
+def success():
+    return render_template('events/thank-you.html', title=(_('Success')))
+
+@bp.route('/q-team/ended', methods=['GET'])
+def ended():
+    return render_template('events/ended.html', title=(_('Ended')))
