1b829cf3093c0cc8f595cd7d997a0a1fba7e19e9
Alex Pauwels <pauwels-a@outlook.com>
2023-05-04 12:43:10 +0100

    Layout changes

--- app/events/qteam/forms.py
+++ app/events/qteam/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, SelectField
+from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, Email
 from flask_babel import _
 
--- app/events/qteam/routes.py
+++ app/events/qteam/routes.py
@@ -10,21 +10,22 @@ from app import db
 @bp.route('/q-team/', methods=['GET', 'POST'])
 def index():
     form = QteamForm()
-    if form.validate_on_submit():
-        entry = QteamEvent(
-            how_many_centers=form.how_many_centers.data,
-            which_type_car=form.which_type_car.data,
-            when_summer_tires=form.when_summer_tires.data,
-            how_many_people=form.how_many_people.data,
-            email=form.email.data,
-            first_name=form.first_name.data,
-            last_name=form.last_name.data,
-            created=datetime.utcnow()
-        )
-        db.session.add(entry)
-        db.session.commit()
-        
-        return redirect(url_for('events.success'))
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            entry = QteamEvent(
+                how_many_centers=form.how_many_centers.data,
+                which_type_car=form.which_type_car.data,
+                when_summer_tires=form.when_summer_tires.data,
+                how_many_people=form.how_many_people.data,
+                email=form.email.data,
+                first_name=form.first_name.data,
+                last_name=form.last_name.data,
+                created=datetime.utcnow()
+            )
+            db.session.add(entry)
+            db.session.commit()
+            
+            return redirect(url_for('events.success'))
     elif request.method == 'GET': 
         return render_template('events/index.html', title=(_('Welcome')), form=form)
 
