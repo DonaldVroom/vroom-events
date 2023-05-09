dafc16afd7043a6e8216daa9f9f0cb015611ad34
Alex Pauwels <pauwels-a@outlook.com>
2023-05-09 18:16:18 +0100

    Fallback font & https redirect

--- app/events/routes.py
+++ app/events/routes.py
@@ -1,5 +1,6 @@
+import os
 from flask_babel import _, get_locale, Babel
-from flask import render_template, g
+from flask import render_template, g, request, redirect
 from app.events import bp
 from app.events.qteam import routes
 from app.events.suzuki import routes
@@ -8,6 +9,11 @@ babel = Babel()
 
 @bp.before_app_request
 def before_request():
+    if 'DYNO' in os.environ: # Only runs when on heroku
+            if request.url.startswith('http://'):
+                url = request.url.replace('http://', 'https://', 1)
+                code = 301
+                return redirect(url, code=code)
     g.locale = str(get_locale())
 
 @bp.context_processor
