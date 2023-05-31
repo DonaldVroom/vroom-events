463e4d555c9b84e2ef67a4793cba7defc93122fc
Alex Pauwels <pauwels-a@outlook.com>
2023-05-31 12:58:05 +0100

    translations

--- app/events/qteam/forms.py
+++ app/events/qteam/forms.py
@@ -6,7 +6,7 @@ from flask_babel import _, lazy_gettext as _l
 
 class QteamForm(FlaskForm):
     how_many_centers = StringField(_l('Hoeveel Q Team service centers zijn er in België aanwezig?'), validators=[DataRequired()])
-    which_type_car = StringField(_l('Met welk type wagen kan je bij een Q Team service center terecht?'), validators=[DataRequired()])
+    which_type_car = StringField(_l('Met welk type wagen kunt u bij een Q Team service center terecht?'), validators=[DataRequired()])
     when_summer_tires = StringField(_l('Vanaf wanneer is het aangeraden om op zomerbanden om te schakelen?'), validators=[DataRequired()])
     how_many_people = IntegerField(
         _l('Hoeveel personen zullen aan deze competitie deelnemen?'),
